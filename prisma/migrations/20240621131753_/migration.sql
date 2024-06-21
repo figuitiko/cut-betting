931c33a1d533f4f918c112690e76d3cd60e9c446
frank Freeman <frankfigao@gmail.com>
2024-06-21 07:18:28 -0600

    refactor: Drop column 'tournamentCBId' from 'BetCB' table
    This commit drops the 'tournamentCBId' column from the 'BetCB' table. The column is no longer needed and all data in the column will be lost. This change improves the database schema by removing unnecessary data.
    
    Note: The commit message has been generated based on the provided code changes and recent user commits.

---
+++ prisma/migrations/20240621131753_/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `tournamentCBId` on the `BetCB` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "BetCB" DROP COLUMN "tournamentCBId";
