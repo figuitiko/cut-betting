ee3dae6b62fa21ee5394a5ef8a02107dda74e499
frank Freeman <frankfigao@gmail.com>
2024-06-22 21:17:27 -0600

    adding revalidate 10 seconds

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,6 +1,7 @@
 import Await from "@/components/Await";
 import RankWrapper from "@/components/home/rank-wrapper";
 import { getPlayers } from "@/lib/actions";
+export const revalidate = 10;
 
 export default function Home() {
   const players = getPlayers();
